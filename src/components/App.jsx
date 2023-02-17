17e1375a40bc9ed9a5b4e003e9c1ecc7a572f2c7
Polyhalov <apolyhalov@gmail.com>
2023-02-17 17:13:12 +0200

    1task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import user from 'user.json'
+import { Profile } from './profile/Profile';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile items={user}/>
     </div>
   );
 };
+   // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
